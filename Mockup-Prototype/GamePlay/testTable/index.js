281f1608c326d27aec9516d4524ffbea831e5e33
Nguyen Van Minh Tuan <tuan.nvm4994@gmail.com>
2020-01-13 15:25:35 +0700

    ahihi

--- Mockup-Prototype/GamePlay/testTable/index.js
+++ Mockup-Prototype/GamePlay/testTable/index.js
@@ -1,29 +1,30 @@
 const sandbox = document.getElementById('sandbox');
 const tagTD = document.getElementsByTagName('td');
 const tagTR = document.getElementsByTagName('tr');
-
 const btnAddRow = document.getElementById('btn-add-row');
 const btnAddCol = document.getElementById('btn-add-col');
 
 let firstTable = 10;
 
-for(let i = 0; i < firstTable; i++) {
+for (let i = 0; i < firstTable; i++) {
     let row = sandbox.insertRow(i);
-    for(let i = 0; i < firstTable; i++) {
-        row.insertCell(0);
+    for (let i = 0; i < firstTable; i++) {
+        let temp = row.insertCell(0);
+        temp.className = "cells";
     }
 }
 
 const createEmptyBox = () => {
-    for(let i = 0; i < tagTD.length; i++) {
+    for (let i = 0; i < tagTD.length; i++) {
         tagTD[i].style.border = '1px solid black';
     }
 }
 
 btnAddRow.onclick = () => {
     let row = sandbox.insertRow(tagTR.length);
-    for(let i = 0; i < firstTable; i++) {
-        row.insertCell(0);
+    for (let i = 0; i < firstTable; i++) {
+        let temp = row.insertCell(0);
+        temp.className = "cells";
     }
     createEmptyBox();
     hoverEachEmptyBox();
@@ -31,8 +32,9 @@ btnAddRow.onclick = () => {
 
 btnAddCol.onclick = () => {
     firstTable++;
-    for(let i = 0; i < tagTR.length; i++) {
-        tagTR[i].insertCell(0);
+    for (let i = 0; i < tagTR.length; i++) {
+        let temp = tagTR[i].insertCell(0);
+        temp.className = "cells";
     }
     createEmptyBox();
     hoverEachEmptyBox();
