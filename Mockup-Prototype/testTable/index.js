b5d5529b93f182d98f53f174e8b7add793a42308
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-01-13 14:01:26 +0700

    update
    Signed-off-by: Nguyễn Bình An <nguyenbinhanltv@gmail.com>

--- Mockup-Prototype/testTable/index.js
+++ Mockup-Prototype/testTable/index.js
@@ -5,38 +5,42 @@ const tagTR = document.getElementsByTagName('tr');
 const btnAddRow = document.getElementById('btn-add-row');
 const btnAddCol = document.getElementById('btn-add-col');
 
-for(let i = 0; i < 5; i++) {
+let firstTable = 10;
+
+for(let i = 0; i < firstTable; i++) {
     let row = sandbox.insertRow(i);
-    for(let i = 0; i < 5; i++) {
+    for(let i = 0; i < firstTable; i++) {
         row.insertCell(0);
     }
 }
 
-for(let i = 0; i < tagTD.length; i++) {
-    tagTD[i].style.border = '1px solid black';
-    tagTD[i].setAttribute('class', 'hover-each-rect');
+const createEmptyBox = () => {
+    for(let i = 0; i < tagTD.length; i++) {
+        tagTD[i].style.border = '1px solid black';
+    }
 }
-
-let count = 5;
 
 btnAddRow.onclick = () => {
     let row = sandbox.insertRow(tagTR.length);
-    for(let i = 0; i < count; i++) {
+    for(let i = 0; i < firstTable; i++) {
         row.insertCell(0);
     }
-    for(let i = 0; i < tagTD.length; i++) {
-        tagTD[i].style.border = '1px solid black';
-        tagTD[i].setAttribute('class', 'hover-each-rect');
-    }
+    createEmptyBox();
+    hoverEachEmptyBox();
 }
 
 btnAddCol.onclick = () => {
-    count++;
+    firstTable++;
     for(let i = 0; i < tagTR.length; i++) {
         tagTR[i].insertCell(0);
     }
-    for(let i = 0; i < tagTD.length; i++) {
-        tagTD[i].style.border = '1px solid black';
-        tagTD[i].setAttribute('class', 'hover-each-rect');
+    createEmptyBox();
+    hoverEachEmptyBox();
+}
+
+const hoverEachEmptyBox = () => {
+    for (let i = 0; i < tagTD.length; i++) {
+        tagTD[i].onmouseenter = () => tagTD[i].style.background = 'black';
+        tagTD[i].onmouseleave = () => tagTD[i].style.background = 'white';
     }
 }
